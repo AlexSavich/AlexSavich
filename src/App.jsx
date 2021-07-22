4704e398e1300ef57d72608feec0665b69de5270
AlexSavih <ae.savich@gmail.com>
2021-07-22 20:15:19 +0300

    login page fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,21 @@
-import React, { useState } from "react";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import AppContext from './AppContext';
+import LoginPage from './LoginPage';
 
 export default function App() {
-    const [login, setLogin] = useState("");
-    const [password, setPassword] = useState("");
-
-    const loginSetter = (event) => {
-        setLogin(event.target.value);
-    };
-
-    const passwordSetter = (event) => {
-        setPassword(event.target.value);
-    };
-
-
-    const toConsole = () => {
-        console.log(password);
-        console.log(login);
-    };
-
-    return (
-        <div
-            style={{
-                width: "100%",
-                maxWidth: "400px",
-                display: "flex",
-                flexDirection: "column",
-            }}
-        >
-            <input
-                style={{ marginBottom: "20px" }}
-                value={login}
-                onChange={loginSetter}
-                placeholder="Login"
-                type="text"
-            />
-            <input
-                style={{ marginBottom: "20px" }}
-                value={password}
-                onChange={passwordSetter}
-                placeholder="Password"
-                type="password"
-            />
-            <button onClick={(e) => toConsole()}>Login</button>
-        </div>
-    );
+  return (
+    <AppContext>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route>
+            404 not found <Link to="/login">login</Link>
+          </Route>
+        </Switch>
+      </Router>
+    </AppContext>
+  );
 }
